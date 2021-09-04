2a0a20a7fffd1387199ca78c3e4e8698b008c945
PauloCNB <paulocezarnb@gmail.com>
2021-09-04 17:12:10 -0300

    01

---
+++ _javascript/01.js
@@ -0,0 +1,18 @@
+//comentario de linha
+
+//console.log('Olá mundo!!!');
+
+//var nome = 'Paulo Cezar';
+//var sobrenome = 'Nobrega Bezerra'
+//var nomecompleto = nome + sobrenome;
+//console.log(nomecompleto);
+//var idade = '19';
+//var complemento = 'anos'
+//var idadecomplemento = idade + complemento;
+//console.log(idadecomplemento);
+
+var valorum = 10;
+var valordois = 2;
+var total;
+/*total = valorum + valordois;
+console.log(total);*/
---
+++ _javascript/02.js
@@ -0,0 +1,10 @@
+var numeroum = 4;
+var numerodois = 5;
+var numerotres = 9;
+var total;
+/*total = (numeroum + numerodois) * numerotres;
+console.log(total);*/
+/*total = numeroum ** numerodois;
+console.log(total)*/
+total = numerotres % numerodois;
+console.log(total)
